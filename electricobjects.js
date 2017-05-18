26a57f686b1547efd784f5981fcc82791aaff419
Chris Tan <csytan@gmail.com>
2017-05-18 11:35:45 -0400

    First pass at the Connected Art lib

---
+++ electricobjects.js
@@ -0,0 +1,47 @@
+(function() {
+
+var ElectricObjects = {};
+
+ElectricObjects.fetchDeviceInfo = function(sample) {
+    sample = Boolean(sample);
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var eoDeviceToken = urlParams.get('eo_device_token');
+
+    if (!sample && !eoDeviceToken) {
+        console.warn('URL parameter "eo_device_token" not found. Falling back to sample mode.');
+        sample = true;
+    }
+
+    var endpointUrl = sample ? 
+        'https://art-api.electricobjects.com/devices/sample' : 
+        'https://art-api.electricobjects.com/devices';
+
+    return fetch(endpointUrl, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            token: eoDeviceToken
+        })
+    })
+        .then(function(response) {
+            if (response.status !== 200) {
+                console.error('Error fetching device info: ' + response.status);  
+                return;
+            }
+
+            return response.json();
+        })
+        .then(function(data) {
+            return data.data;
+        });
+};
+
+window.ElectricObjects = ElectricObjects;
+
+})();
+
